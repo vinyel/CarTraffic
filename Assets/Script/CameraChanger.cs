e7d008fe556533db93385ae15041b1859870e5bf
U-VAIO\apple <vinyel422@gmail.com>
2018-07-16 19:37:23 +0900

    add function

---
+++ Assets/Script/CameraChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour {
+    [SerializeField]private GameObject mainCamera;
+    [SerializeField]private GameObject otherCamera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("1")) {
+            mainCamera.SetActive(!mainCamera.activeSelf);
+            otherCamera.SetActive(!otherCamera.activeSelf);
+        }
+	}
+}
---
+++ Assets/Script/GetSpeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSpeed : MonoBehaviour {
+    private Rigidbody _rigidbody;
+	// Use this for initialization
+	void Start () {
+        GameObject carObject = GameObject.Find("CarWaypointBased");
+        _rigidbody = carObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            // 速度ベクトルを表示
+            Debug.Log("速度ベクトル: " + _rigidbody.velocity);
+
+            // 速度を表示
+            Debug.Log("速度: " + _rigidbody.velocity.magnitude);
+        }
+    }
+}
